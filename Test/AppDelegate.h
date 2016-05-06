09734ae3fd017862745d9ed774840bee3dfcfd6e
YMZ <517053480@qq.com>
2016-05-06 17:19:43 +0800

    第一次操作

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by YMZ on 16/5/6.
+//  Copyright © 2016年 LZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by YMZ on 16/5/6.
+//  Copyright © 2016年 LZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
